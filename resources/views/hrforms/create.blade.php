90b0a119f0738f9402b2b40b2f2785151b78cb7d
Drew Allen <drwllnrmz@gmail.com>
2025-08-08 16:03:07 +0800

    style(hrforms-create): save button adjusted

--- resources/views/hrforms/create.blade.php
+++ resources/views/hrforms/create.blade.php
@@ -54,11 +54,9 @@
                     </div>
                 </div>
                 <div class="modal-footer px-4">
-                    <button type="submit" class="btn btn-primary"
-                        style="background-color: #003566; border-color: #003566;">
-                        Save Form
+                    <button type="submit" class="btn" style="background-color: #003566; color: white;">
+                        <i class="fas fa-save mx-1"></i> Save Form
                     </button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                 </div>
             </div>
         </form>
