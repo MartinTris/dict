321473e6546e996eef14b4f2836caab954ba018b
Drew Allen <drwllnrmz@gmail.com>
2025-08-08 15:55:27 +0800

    style(fw4a-create): modified save button and removed cancel button

--- resources/views/connect/fw4a/create.blade.php
+++ resources/views/connect/fw4a/create.blade.php
@@ -26,7 +26,7 @@
                         <label>Region</label>
                         <select name="region_id" id="region" class="form-select" required>
                             <option value="">Select Region</option>
-                            @foreach($regions as $region)
+                            @foreach ($regions as $region)
                                 <option value="{{ $region->id }}">{{ $region->region_code }}</option>
                             @endforeach
                         </select>
@@ -85,10 +85,10 @@
                     </div>
                 </div>
                 <div class="modal-footer px-4">
-                    <button type="submit" class="btn btn-primary" style="background-color: #003566; border-color: #003566;">
-    Save Site
-</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn" style="background-color: #003566; color: white;">
+                        <i class="fas fa-save mx-1"></i> Save 
+                    </button>
+                    
                 </div>
             </div>
         </form>
@@ -106,8 +106,8 @@
                 </div>
                 <div class="modal-body">
                     <div class="mb-3">
-                        <input type="text" name="district_name" class="form-control" placeholder="Enter District Name"
-                            required>
+                        <input type="text" name="district_name" class="form-control"
+                            placeholder="Enter District Name" required>
                         <input type="hidden" name="province_id" id="districtProvinceId">
                     </div>
                 </div>
@@ -131,8 +131,8 @@
                 </div>
                 <div class="modal-body">
                     <div class="mb-3">
-                        <input type="text" name="locality_name" class="form-control" placeholder="Enter Locality Name"
-                            required>
+                        <input type="text" name="locality_name" class="form-control"
+                            placeholder="Enter Locality Name" required>
                         <input type="hidden" name="district_id" id="localityDistrictId">
                     </div>
                 </div>
@@ -152,34 +152,36 @@
             }
         });
 
-        $('#region').on('change', function () {
+        $('#region').on('change', function() {
             let regionId = $(this).val();
             $('#province').html('<option value="">Loading...</option>').prop('disabled', true);
             $('#district').html('<option value="">Select District</option>').prop('disabled', true);
             $('#locality').html('<option value="">Select Locality</option>').prop('disabled', true);
 
             if (regionId) {
-                $.get(`/get-provinces/${regionId}`, function (data) {
+                $.get(`/get-provinces/${regionId}`, function(data) {
                     $('#province').html('<option value="">Select Province</option>');
                     data.forEach(p => {
-                        $('#province').append(`<option value="${p.id}">${p.province_name}</option>`);
+                        $('#province').append(
+                            `<option value="${p.id}">${p.province_name}</option>`);
                     });
                     $('#province').prop('disabled', false);
                 });
             }
         });
 
-        $('#province').on('change', function () {
+        $('#province').on('change', function() {
             let provinceId = $(this).val();
             $('#district').html('<option value="">Loading...</option>').prop('disabled', true);
             $('#locality').html('<option value="">Select Locality</option>').prop('disabled', true);
 
             if (provinceId) {
                 $('#districtProvinceId').val(provinceId);
-                $.get(`/get-districts/${provinceId}`, function (data) {
+                $.get(`/get-districts/${provinceId}`, function(data) {
                     $('#district').html('<option value="">Select District</option>');
                     data.forEach(d => {
-                        $('#district').append(`<option value="${d.id}">${d.district_name}</option>`);
+                        $('#district').append(
+                            `<option value="${d.id}">${d.district_name}</option>`);
                     });
                     $('#district').append(`<option value="add_new_district">+ Add New District</option>`);
                     $('#district').prop('disabled', false);
@@ -187,7 +189,7 @@
             }
         });
 
-        $('#district').on('change', function () {
+        $('#district').on('change', function() {
             let districtId = $(this).val();
             $('#localityDistrictId').val(districtId);
 
@@ -198,10 +200,11 @@
             }
 
             if (districtId) {
-                $.get(`/get-localities/${districtId}`, function (data) {
+                $.get(`/get-localities/${districtId}`, function(data) {
                     $('#locality').html('<option value="">Select Locality</option>');
                     data.forEach(l => {
-                        $('#locality').append(`<option value="${l.id}">${l.locality_name}</option>`);
+                        $('#locality').append(
+                            `<option value="${l.id}">${l.locality_name}</option>`);
                     });
                     $('#locality').append(`<option value="add_new_locality">+ Add New Locality</option>`);
                     $('#locality').prop('disabled', false);
@@ -209,16 +212,16 @@
             }
         });
 
-        $('#locality').on('change', function () {
+        $('#locality').on('change', function() {
             if ($(this).val() === 'add_new_locality') {
                 $('#addLocalityModal').modal('show');
                 $(this).val('');
             }
         });
 
-        $('#addDistrictForm').on('submit', function (e) {
+        $('#addDistrictForm').on('submit', function(e) {
             e.preventDefault();
-            $.post('/districts', $(this).serialize(), function (res) {
+            $.post('/districts', $(this).serialize(), function(res) {
                 $('#district').append(`<option value="${res.id}">${res.district_name}</option>`);
                 $('#district').val(res.id);
                 $('#addDistrictModal').modal('hide');
@@ -226,9 +229,9 @@
             });
         });
 
-        $('#addLocalityForm').on('submit', function (e) {
+        $('#addLocalityForm').on('submit', function(e) {
             e.preventDefault();
-            $.post('/localities', $(this).serialize(), function (res) {
+            $.post('/localities', $(this).serialize(), function(res) {
                 $('#locality').append(`<option value="${res.id}">${res.locality_name}</option>`);
                 $('#locality').val(res.id);
                 $('#addLocalityModal').modal('hide');
