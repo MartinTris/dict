5846838bf95f2557faa27011e3e8cc731b7599ff
Omphile Ruoxi <dict.local.server@gmail.com>
2025-08-11 15:49:13 +0800

    feat(Server Starter): added starter for laragon for the mysql server

--- server_start.bat
+++ server_start.bat
@@ -3,7 +3,7 @@ REM Get local IP (IPv4)
 FOR /F "tokens=14" %%i IN ('ipconfig ^| findstr "IPv4" ^| findstr /V "127.0.0.1"') DO SET LANIP=%%i
 
 REM Show detected IP
-echo Detected LAN IP: %LANIP%
+echo LAN IP: %LANIP%
 
 REM Update APP_URL in .env
 powershell -Command "(Get-Content .env) -replace 'APP_URL=.*', 'APP_URL=http://%LANIP%:8000' | Set-Content .env"
@@ -11,6 +11,12 @@ powershell -Command "(Get-Content .env) -replace 'APP_URL=.*', 'APP_URL=http://%
 REM Clear Laravel cache
 php artisan config:clear
 php artisan cache:clear
+php artisan config:cache
+
+echo Starting MySQL Service...
+start "" "C:\laragon\laragon.exe"
+
+echo Access the system here: https://bit.ly/dict-4a
 
 REM Start Laravel server on LAN IP
 php artisan serve --host=%LANIP% --port=8000
